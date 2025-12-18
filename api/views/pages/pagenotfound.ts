29a0b162e683c69a73867e9ee3a3a6305aa769a3
nineisten <nineisx@icloud.com>
2025-12-18 01:39:46 -0500

    build:404 redirect

---
+++ api/views/pages/pagenotfound.ts
@@ -0,0 +1,23 @@
+import Link from "../components/elements/link"
+import Section from "../components/elements/section"
+
+
+const css = /*css*/`
+
+`
+function pageNotFound(){
+    const html = /*html*/`
+    <style>${css}</style>
+    ${Section({
+        content:/*html*/`
+            <!-- start section-->
+            <h1>Page Not Found </h1>
+            <h4>This page has been moved, or does not exist.</h4>
+           
+            <!-- end section-->
+        `,class:'flex_columns'
+    })}
+    `
+    return html
+}
+export default pageNotFound
